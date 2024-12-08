44816609778d266c3f1d96665d32fc495944e2cb
Saurabh Gurde <saurabhgurde@gmail.com>
2024-12-08 12:38:39 +0530

    vercel.json changes

---
+++ server/api/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRouter from "../routes/user.js";
+import dataRouter from "../routes/data.js";
+import fetch from "../middleware/auth.js";
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.use("/user", userRouter);
+app.use("/data", fetch, dataRouter);
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+export default app
